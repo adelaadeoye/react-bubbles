89ad264b16bfeccbbcb9e3e012f930b87f4f2cad
adeoye adela <adeoye.adela@gmail.com>
2019-10-18 13:12:40 +0100

    color List diplay properly

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import axiosWithAuth from '../utils/axiosWithAuth'
+
+
+
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  const token= localStorage.getItem(token)
+useEffect(()=>{
+ 
+  axiosWithAuth()
+  .get('http://localhost:5000/api/colors', token)
+    .then(res => {
+      
+      console.log(res.data);
+      setColorList(res.data)
+    }).catch(err => console.log(err.response));
+},[])
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
